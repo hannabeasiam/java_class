0a8f6a67403e1ce3e58978c04d180d12311215a1
hannabeasiam <hannabeasiam@gmail.com>
2017-01-19 20:01:33 -0500

    Update Rectangle.java
    This is additional comment from the class 
    * Design mutator & accessor 
    (1. security 2. make better environment to controll)

--- exercise1/Rectangle.java
+++ exercise1/Rectangle.java
@@ -5,11 +5,30 @@ public class Rectangle {
 	private double length;
 	private double width;
 	
+	public static void main(String[] args)
+	{
+		Rectangle R1 = new Rectangle();
+		Rectangle R2 = new Rectangle();
+		//R1
+		R1.setLength(4);
+		R1.setWidth(5);
+		//R2
+		R2.setLength(3);
+		R2.setWidth(11);
+		//First Rectangle R1
+		System.out.println("length is "+R1.getLength());
+		System.out.println("width is "+R1.getWidth());	
+		System.out.println("Area is "+R1.getArea());
+		//Second Rectangle R2
+		System.out.println("length is "+R2.getLength());
+		System.out.println("width is "+R2.getWidth());	
+		System.out.println("Area is "+R2.getArea());
+	}
 	 /**
 	   The setLength method stores a value in the length field
 	   @param len the value to store in length
 	  */
-	//setLength is mutator(setter)
+	//setLength is mutator(setter)-change value
 	//mutator method that stored a value in a field or changes the value of a field
 	public void setLength(double len)
 	{
@@ -20,9 +39,12 @@ public class Rectangle {
 	  * The setLength method stores a value in the width field
 	  * @param w the value to store in width
 	  */
+	//public void setField(fieldType val){fieldVariable=val;}
 	public void setWidth(double w)
 	{
-		width = w;
+		//validate mutator, with mutator we can controll value
+		if(wi>=0)
+			width = w;
 	}
 	
 	/**
@@ -41,6 +63,7 @@ public class Rectangle {
 	 * the getWidth method returns a Rectangle object's width
 	 * @return the value in the width field
 	 * */
+	//Accessors Templete public fieldType getField(){return fieldVarible;}
 	public double getWidth()
 	{
 		return width;
